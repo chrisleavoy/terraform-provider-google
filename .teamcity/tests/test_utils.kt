f04508349bfe02f44f0bd7bf7206f4954e9a9ac0
Sarah French <sarah.french@hashicorp.com>
2024-01-12 14:30:05 +0000

    Update tests to be compatible with refactoring

--- .teamcity/tests/test_utils.kt
+++ .teamcity/tests/test_utils.kt
@@ -1,15 +1,49 @@
-///*
-// * Copyright (c) HashiCorp, Inc.
-// * SPDX-License-Identifier: MPL-2.0
-// */
-//
-//// this file is copied from mmv1, any changes made here will be overwritten
-//
-//package tests
-//
-//import builds.AccTestConfiguration
-//import jetbrains.buildServer.configs.kotlin.AbsoluteId
-//
-//fun testConfiguration(): AccTestConfiguration {
-//    return AccTestConfiguration("billingAccount", "billingAccount2", "credentials", "custId", "firestoreProject", "identityUser", "masterBillingAccount", "org", "org2", "orgDomain", "project", "projectNumber", "region", "serviceAccount", "zone")
-//}
+/*
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// this file is copied from mmv1, any changes made here will be overwritten
+
+package tests
+
+import builds.AllContextParameters
+
+const val gaProjectName = "Google"
+const val betaProjectName = "Google Beta"
+const val nightlyTestsProjectName = "Nightly Tests"
+
+fun testContextParameters(): AllContextParameters {
+    return AllContextParameters(
+        "credsGa",
+        "credsBeta",
+        "credsVcr",
+        "serviceAccountGa",
+        "serviceAccountBeta",
+        "serviceAccountVcr",
+        "projectGa",
+        "projectBeta",
+        "projectVcr",
+        "projectNumberGa",
+        "projectNumberBeta",
+        "projectNumberVcr",
+        "identityUserGa",
+        "identityUserBeta",
+        "identityUserVcr",
+        "firestoreProjectGa",
+        "firestoreProjectBeta",
+        "firestoreProjectVcr",
+"masterBillingAccountGa",
+        "masterBillingAccountBeta",
+        "masterBillingAccountVcr",
+        "org2Ga",
+        "org2Beta",
+        "org2Vcr",
+        "billingAccount",
+        "billingAccount2",
+        "custId",
+        "org",
+        "orgDomain",
+        "region",
+        "zone")
+}
